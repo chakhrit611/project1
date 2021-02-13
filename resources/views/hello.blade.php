57246cea2d42013aac8c46ba3d24d2a0d2716517
chakhrit <6112389@g.cmru.ac.th>
2021-02-13 14:08:29 +0700

    ส่งข้อมูลviewผ่านcontroller

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/about">about</a> |<a href="/contact">contact</a> | <a href="/welcome">index</a><br>
+    <h1>รักนะeiei{{$name}}  อายุ  {{$age}} ขวบ</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/welcome', [HomeController::class, 'index']);
 Route::get('/about', [HomeController::class, 'about']);
 Route::get('/contact', [HomeController::class, 'contact']);
-Route::get('/สวัสดี/{name}', function ($name) {
-    return "สวัสดี $name";
-});
-Route::get('/calculater/{num1}/{num2}', function ($num1,$num2) {
-    return "ผลบวกของเลข$num1 และ $num2 = " . ($num1+$num2);
-});
+Route::get('/hello/{name}/{age}', [HomeController::class, 'hello']);
