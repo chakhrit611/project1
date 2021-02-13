c51db2df61a7012b01657666e58a83175414411b
chakhrit <6112389@g.cmru.ac.th>
2021-02-13 14:28:15 +0700

    masterlayout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <a href="/about">about</a> |<a href="/contact">contact</a> | <a href="/welcome">index</a><br>
+@extends('layout.master')
+@section('content')
     <h1>รักนะabout</h1>
-</body>
-</html>
+@endsection
